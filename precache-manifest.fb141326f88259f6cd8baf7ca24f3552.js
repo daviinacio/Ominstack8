7ef1932e392a19a32b0381ebb100a4dae3d6cb41
Davi Inácio <aazz6850@gmail.com>
2020-03-23 22:37:19 -0300

    Updates

---
+++ precache-manifest.fb141326f88259f6cd8baf7ca24f3552.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be4b829b38e0d7141d91c3ab48ba18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d713c1916d6d0ff220e2",
+    "url": "/static/css/main.c02c08d3.chunk.css"
+  },
+  {
+    "revision": "90d458614a5291d5ba75",
+    "url": "/static/js/2.2eec4bf1.chunk.js"
+  },
+  {
+    "revision": "461cae16d5efcce0affb54cb7c40afdf",
+    "url": "/static/js/2.2eec4bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d713c1916d6d0ff220e2",
+    "url": "/static/js/main.a4518cda.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "21c10b2613fbc1b8b36859e433819ed1",
+    "url": "/static/media/itsamatch.21c10b26.png"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/static/media/logo.6a01ceef.svg"
+  }
+]);
